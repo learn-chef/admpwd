8bc151e39a6c6b29cda0cc93358209dde9bc4454
tpetchel <tpetchel@gmail.com>
2017-02-13 10:46:43 -0500

    Initial

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft Services\AdmPwd' do
+  values [{
+    name: 'PasswordComplexity',
+    type: :dword,
+    data: 4
+  }]
+  action :create
+  recursive true
+end
